c6f4179e99563beb9d728cbab0ba4591d011f842
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-12-07 21:17:12 -0800

    created server

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+//;/  (O_O) /;//
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (request, response) => {
+  response.send('WERE MY ANIMALS AT YO!?!?');
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
