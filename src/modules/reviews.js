26d35f659d0bf771d06ef98e062eef0c945ddeb2
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-12-14 22:10:11 -0800

    working on CRUD for Reviews page

--- src/modules/reviews.js
+++ src/modules/reviews.js
@@ -1,47 +1,58 @@
-// const ReviewModel = require('../models/reviews');
+const ReviewModel = require('../models/reviews');
 
-// async function getReviews(request, response, next) {
-//   console.log(request.user.email);
-//   try {
-//     const reivews = await ReviewModel.find({ email: request.user.email });
-//     response.status(200).send(books);
-//   } catch (error) {
-//     console.error(error);
-//     next(error);
-//   }
-// }
 
-// const deleteReview = async (request, response, next) => {
-//   try {
+async function getReviews(request, response, next) {
+  console.log(request.user.email);
+  try {
+    const reviews = await ReviewModel.find({ email: request.user.email });
+    response.status(200).send(reviews);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+async function createReviews(request, response, next) {
+  console.log(request.user.email);
+  try {
+    const reviews = await ReviewModel.create({ email: request.user.email });
+    response.status(200).send(reviews);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
 
-//     const reviewToBeDeleted = await ReviewModel.findOne({ _id: request.params.id, email: request.user.email });
-//     if (!reviewToBeDeleted) response.status(404).send('Unable to find that review to delete');
-//     else {
-//       await ReviewModel.findByIdAndDelete(request.params.id);
-//       response.status(200).send('Review was sucessfully deleted');
-//     }
-//   } catch (error) {
-//     error.customMessage = 'Something went wrong with deleting your Review!';
-//     console.error(error.customMessage + error);
-//     next(error);
-//   }
-// };
+const deleteReview = async (request, response, next) => {
+  try {
 
-// let updatedReview = async (request, response, next) => {
-//   try {
-//     const updatedReview = await ReviewModel.findByIdAndUpdate(request.params.id, request.body, { new: true });
-//     response.status(200).send(updatedReview);
+    const reviewToBeDeleted = await ReviewModel.findOne({ _id: request.params.id, email: request.user.email });
+    if (!reviewToBeDeleted) response.status(404).send('Unable to find that review to delete');
+    else {
+      await ReviewModel.findByIdAndDelete(request.params.id);
+      response.status(200).send('Review was sucessfully deleted');
+    }
+  } catch (error) {
+    error.customMessage = 'Something went wrong with deleting your Review!';
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
 
-//   } catch (error) {
-//     error.customMessage = 'Something went wrong with updating your Review!';
-//     console.error(error.customMessage + error);
-//     next(error);
-//   }
-// };
+let updatedReview = async (request, response, next) => {
+  try {
+    const updatedReview = await ReviewModel.findByIdAndUpdate(request.params.id, request.body, { new: true });
+    response.status(200).send(updatedReview);
 
-// let handleGetUser = async (request, response) => {
-//   console.log('Getting the user');
-//   response.send(request.user);
-// };
+  } catch (error) {
+    error.customMessage = 'Something went wrong with updating your Review!';
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
 
-// module.exports = { getReviews, deleteReview, updatedReview, handleGetUser };
+let handleGetUser = async (request, response) => {
+  console.log('Getting the user');
+  response.send(request.user);
+};
+
+module.exports = { getReviews, deleteReview, updatedReview, handleGetUser };
